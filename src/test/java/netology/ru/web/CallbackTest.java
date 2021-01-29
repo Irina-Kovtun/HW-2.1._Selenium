64a568500cf55c1fb78c4e535979bcaf4eaaa74a
Irina-Kovtun <irinakovtun.us@gmail.com>
2021-01-29 15:29:59 +0300

    Appveyor is added

--- src/test/java/netology/ru/web/CallbackTest.java
+++ src/test/java/netology/ru/web/CallbackTest.java
@@ -18,11 +18,13 @@ public class CallbackTest {
     private WebDriver driver;
 
     @BeforeAll
+    //running the driver
     static void setUpAll() {
         System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
     }
 
     @BeforeEach
+        //creating driver object
     void setUp() {
         driver = new ChromeDriver();
     }
@@ -33,18 +35,21 @@ public class CallbackTest {
         driver = null;
     }
 
-    //    @Test
-//    void shouldTestSomething() {
-//        driver.get("http://localhost:9999");
-//    }
     @Test
     void shouldTestV1() {
+        //running the form at URL
         driver.get("http://localhost:9999");
+        //entering first name
         driver.findElement(By.cssSelector("[data-test-id =name]")).sendKeys("Василий");
+        //entering phone
         driver.findElement(By.cssSelector("[data-test-id =phone]")).sendKeys("+77777777777");
+        //clicking checkbox
         driver.findElement(By.cssSelector("[data-test-id =agreement]")).click();
+        //clicking sign up
         driver.findElement(By.tagName("button")).click();
+        //getting message after the form is submitted
         String actualText  = driver.findElement(By.cssSelector("[data-test-id]")).getText();
+        //assering to expectations
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", actualText.trim());
     }
 }
